2c72dec626cc2e9d00cf457e3309ba3d3db02e69
kornelijusp <kornelijus.petronis2000@gmail.com>
2020-03-05 00:59:44 +0200

    3.2.5v

--- Source-2.0v.cpp
+++ Source-2.0v.cpp
@@ -91,12 +91,21 @@ double Kandidatas::getNorimasAtlig()
 // Prototipai
 double vidurkis(Kandidatas[], int);
 bool skaityti(Kandidatas[], int &);
+void paieska(Kandidatas[], int, Kandidatas[], int &, string, string, string, int, string);
+void removeDupWord(char[], string[]);
+void pildyti(Kandidatas[], int);
 
 int main()
 {
     Kandidatas kandidatai[maxStudent];
-    int n;
-    string state;
+    int n, darboPatirtis_t, kiek;
+    string state, x;
+    string vardas_t, pavarde_t, profesija_t;
+    double norimasAtlig_t;
+    Kandidatas kandidatuSarasas[maxStudent];
+    size_t pos = 0;
+    // string
+
     if (skaityti(kandidatai, n))
     {
         do
@@ -110,19 +119,79 @@ int main()
             cout << "5. Perziureti visus irasus" << endl;
             cout << "6. Baigti.\n\n:";
             cin >> state;
+            cout << "-------------------------------------\n";
 
             switch (stoi(state))
             {
                 // Vidurkis
             case 1:
                 cout << "Vidurkis: " << vidurkis(kandidatai, n) << endl;
+                break;
+            case 2:
+                cout << "Paieska\n";
+                cout << "1. Pagal varda, pavarde\n";
+                cout << "2. Pagal varda, pavarde, profesija, darbo patirti\n";
+                cout << "3. Baigti\n\n:";
+                cin >> x;
+                cout << "-------------------------------------\n";
+                if (!x.compare("1"))
+                {
+                    cout << "\nIvesk varda ir pavarde" << endl;
+                    cin >> vardas_t >> pavarde_t;
+                    cout << "=====================================\n";
+                    paieska(kandidatai, n, kandidatuSarasas, kiek, vardas_t, pavarde_t, profesija_t, darboPatirtis_t, x);
+                    for (int i = 0; i < kiek; i++)
+                    {
+                        cout << "\n"
+                             << i + 1 << ". "
+                             << kandidatuSarasas[i].getVardas() << " ";
+                        cout << kandidatuSarasas[i].getPavarde() << " ";
+                        cout << kandidatuSarasas[i].getProfesija() << " ";
+                        cout << kandidatuSarasas[i].getDarboPatirtis() << " ";
+                        cout << kandidatuSarasas[i].getNorimasAtlig() << endl;
+                    }
+                }
+                else if (!x.compare("2"))
+                {
+                    cout << "\nIvesk varda, pavarde, profesija ir darbo patirti" << endl;
+                    cin >> vardas_t >> pavarde_t >> profesija_t >> darboPatirtis_t;
+                    cout << "=====================================\n";
+                    paieska(kandidatai, n, kandidatuSarasas, kiek, vardas_t, pavarde_t, profesija_t, darboPatirtis_t, x);
+                    for (int i = 0; i < kiek; i++)
+                    {
+                        cout << "\n"
+                             << i + 1 << ". "
+                             << kandidatuSarasas[i].getVardas() << " ";
+                        cout << kandidatuSarasas[i].getPavarde() << " ";
+                        cout << kandidatuSarasas[i].getProfesija() << " ";
+                        cout << kandidatuSarasas[i].getDarboPatirtis() << " ";
+                        cout << kandidatuSarasas[i].getNorimasAtlig() << endl;
+                    }
+                }
+                else
+                    break;
 
+                break;
+            case 3:
+                cout << "\nIveskite studento varda, pavarde, profesija, darbo patirti ir norima atlygi (Atskirti tarpais).\n";
+
+                cin >> vardas_t >> pavarde_t >> profesija_t >> darboPatirtis_t >> norimasAtlig_t;
+                //pridedamas naujas studentas
+                kandidatai[n] = Kandidatas(vardas_t, pavarde_t, profesija_t, darboPatirtis_t, norimasAtlig_t);
+                //irasoma i faila
+                pildyti(kandidatai, n);
+                n++;
+                cout << "\nIrasyta sekmingai!\n";
+                break;
+            case 6:
+                cout << "Programa baigta!\n";
                 break;
 
             default:
                 break;
             }
 
+            cout << "-------------------------------------\n";
         } while (state.compare("6"));
     }
     return 0;
@@ -154,6 +223,7 @@ bool skaityti(Kandidatas kandidatai[], int &n)
             double norimasAtlig_t;
 
             fin >> vardas_t >> pavarde_t >> profesija_t >> darboPatirtis_t >> norimasAtlig_t;
+
             kandidatai[n].setVardas(vardas_t);
             kandidatai[n].setPavarde(pavarde_t);
             kandidatai[n].setProfesija(profesija_t);
@@ -171,4 +241,34 @@ bool skaityti(Kandidatas kandidatai[], int &n)
              << "\n\n";
         return false;
     }
+}
+
+void paieska(Kandidatas kandidatai[], int n, Kandidatas kandidatuSarasas[], int &kiek, string vardas_t, string pavarde_t, string profesija_t, int darboPatirtis_t, string x)
+{
+
+    kiek = 0;
+    for (int i = 0; i < n; i++)
+    {
+        //patikrina varda, pavarde, profesija
+        if (!x.compare("1") && kandidatai[i].getVardas().compare(vardas_t) == 0 && kandidatai[i].getPavarde().compare(pavarde_t) == 0)
+        {
+            kandidatuSarasas[kiek] = kandidatai[i];
+            kiek++;
+        }
+        else if (!x.compare("2") && kandidatai[i].getVardas().compare(vardas_t) == 0 && kandidatai[i].getPavarde().compare(pavarde_t) == 0 && kandidatai[i].getProfesija().compare(profesija_t) == 0 && kandidatai[i].getDarboPatirtis() == darboPatirtis_t)
+        {
+            kandidatuSarasas[kiek] = kandidatai[i];
+            kiek++;
+        }
+    }
+}
+void pildyti(Kandidatas kandidatai[], int n)
+{
+    ofstream fout;
+    fout.open(dataBase, fstream::app);
+
+    fout << "\n"
+         << kandidatai[n].getVardas() << " " << kandidatai[n].getPavarde() << " " << kandidatai[n].getProfesija() << " " << kandidatai[n].getDarboPatirtis() << " " << kandidatai[n].getNorimasAtlig();
+
+    fout.close();
 }
--- main.cpp
+++ main.cpp
@@ -7,55 +7,66 @@ const char duom[] = "duom.txt";
 //apraÅ¡omas maksimalus studentÅ³ kiekis
 const int maxStud = 20;
 //studentÅ³ klasÄ—
-class Studentas {
-  private:
-    //kintamieji apraÅ¡antys studentÄ…
-    string vardas;
-    string pavarde;
-    string grupe;
-    double vidurkis;
-  public:
-    //konstruktoriai
-    Studentas() {
-      vardas = "Nera";
-      pavarde = "Nera";
-      grupe = "Nera";
-      vidurkis = 0;
-    }
+class Studentas
+{
+private:
+  //kintamieji apraÅ¡antys studentÄ…
+  string vardas;
+  string pavarde;
+  string grupe;
+  double vidurkis;
 
-    Studentas(string vardas, string pavarde, string grupe, double vidurkis) {
-      this->vardas = vardas;
-      this->pavarde = pavarde;
-      this->grupe = grupe;
-      this->vidurkis = vidurkis;
-    }
-    //nustatymas individualiÅ³ parametrÅ³
-    void setVardas(string vardas) {
-      this->vardas = vardas;
-    }
-    void setPavarde(string pavarde) {
-      this->pavarde = pavarde;
-    }
-    void setGrupe(string grupe) {
-      this->grupe = grupe;
-    }
-    void setVidurkis(double vidurkis) {
-      this->vidurkis = vidurkis;
-    }
-    //graÅ¾inami objekto parametrai
-    string getVardas() {
-      return vardas;
-    }
-    string getPavarde() {
-      return pavarde;
-    }
-    string getGrupe() {
-      return grupe;
-    }
-    double getVidurkis() {
-      return vidurkis;
-    }
+public:
+  //konstruktoriai
+  Studentas()
+  {
+    vardas = "Nera";
+    pavarde = "Nera";
+    grupe = "Nera";
+    vidurkis = 0;
+  }
 
+  Studentas(string vardas, string pavarde, string grupe, double vidurkis)
+  {
+    this->vardas = vardas;
+    this->pavarde = pavarde;
+    this->grupe = grupe;
+    this->vidurkis = vidurkis;
+  }
+  //nustatymas individualiÅ³ parametrÅ³
+  void setVardas(string vardas)
+  {
+    this->vardas = vardas;
+  }
+  void setPavarde(string pavarde)
+  {
+    this->pavarde = pavarde;
+  }
+  void setGrupe(string grupe)
+  {
+    this->grupe = grupe;
+  }
+  void setVidurkis(double vidurkis)
+  {
+    this->vidurkis = vidurkis;
+  }
+  //graÅ¾inami objekto parametrai
+  string getVardas()
+  {
+    return vardas;
+  }
+  string getPavarde()
+  {
+    return pavarde;
+  }
+  string getGrupe()
+  {
+    return grupe;
+  }
+  double getVidurkis()
+  {
+    return vidurkis;
+  }
 };
 //apsiraÅ¡omos funkcijos
 bool skaityti(Studentas studentai[], int &n);
@@ -66,24 +77,28 @@ void ieskotiStud(Studentas studentai[], int n, Studentas studentuSarasas[], int 
 void ieskotiStud(Studentas studentai[], int n, Studentas studentuSarasas[], int &kiek, string vardasTemp, string pavardeTemp, string grupeTemp);
 void rikiuoti(Studentas studentai[], int n, int vidurkiArPavarde);
 
-int main() {
+int main()
+{
   //kiek rasta studentÅ³
   int n;
   //studentÅ³ masyvas
   Studentas studentai[maxStud];
   //jei pavyko nuskaityti failÄ… programa vyksta toliau
-  if (skaityti(studentai, n)) {
+  if (skaityti(studentai, n))
+  {
     //isspausdina Ä¯ ekranÄ… rastÄ… studentÅ³ saraÅ¡Ä…
     spausdintiStud(studentai, n);
     //klaviatÅ«ros input kintamasis
     string input;
-    do {
+    do
+    {
       cout << "\nKomandos:\n0. Baigti darbÄ…\n1. Geriausio ir blogiausio studento paieÅ¡ka.\n2. PaieÅ¡ka nurodÅ¾ius vardÄ… ir pavardÄ™.\n3. PaieÅ¡ka nurodÅ¾ius vardÄ…, pavardÄ™ ir grupÄ™.\n4. Rikiavimas pagal vidurkÄ¯ arba pavardÄ™.\n5. Pildyti duomenis.\n\n";
       //ivedimas is klaviatÅ«ros navigacijai po meniu
       cout << "Komanda: ";
       cin >> input;
       //tikrina kas buvo Ä¯vesta ir atitinkamai atlieka veiksmus
-      if (input.compare("1") == 0) {
+      if (input.compare("1") == 0)
+      {
         //geru ir blogu studentu masyvas, nes gali bÅ«ti ir keli su tais paÄiais parametrais
         Studentas studentaiGerBlog[maxStud];
         int kiekGeru, kiekBlogu;
@@ -91,20 +106,24 @@ int main() {
         ieskGerBlogStud(studentai, n, studentaiGerBlog, kiekGeru, kiekBlogu);
         //iÅ¡veda Ä¯ ekranÄ… rastus studentus
         cout << "\nGeras(-i) studentas(-ai):" << endl;
-        for (int i = 0; i < kiekGeru; i++) {
+        for (int i = 0; i < kiekGeru; i++)
+        {
           cout << studentaiGerBlog[i].getVardas() << " ";
           cout << studentaiGerBlog[i].getPavarde() << " ";
           cout << studentaiGerBlog[i].getGrupe() << " ";
           cout << studentaiGerBlog[i].getVidurkis() << "\n";
         }
         cout << "\nBlogas(-i) studentas(-ai):" << endl;
-        for (int i = kiekGeru; i < kiekGeru + kiekBlogu; i++) {
+        for (int i = kiekGeru; i < kiekGeru + kiekBlogu; i++)
+        {
           cout << studentaiGerBlog[i].getVardas() << " ";
           cout << studentaiGerBlog[i].getPavarde() << " ";
           cout << studentaiGerBlog[i].getGrupe() << " ";
           cout << studentaiGerBlog[i].getVidurkis() << "\n";
         }
-      } else if (input.compare("2") == 0) {
+      }
+      else if (input.compare("2") == 0)
+      {
         //apsiraÅ¡omas gautÅ³ studentu sÄ…raÅ¡ui saugoti
         Studentas studentuSarasas[maxStud];
         int kiek;
@@ -114,19 +133,25 @@ int main() {
         cout << "\nÄ®veskite vardÄ… ir pavardÄ™ (atskirti tarpu)\n";
         cin >> vardasTemp >> pavardeTemp;
         //suieÅ¡ko studento pagal vardÄ… ir pavardÄ™
-        ieskotiStud(studentai, n, studentuSarasas, kiek, vardasTemp,pavardeTemp);
-        if (kiek > 0) {
+        ieskotiStud(studentai, n, studentuSarasas, kiek, vardasTemp, pavardeTemp);
+        if (kiek > 0)
+        {
           cout << "\nRasti studentas(-ai) pagal vardÄ… ir pavardÄ™:\n";
-          for (int i = 0; i < kiek; i++) {
+          for (int i = 0; i < kiek; i++)
+          {
             cout << studentuSarasas[i].getVardas() << " ";
             cout << studentuSarasas[i].getPavarde() << " ";
             cout << studentuSarasas[i].getGrupe() << " ";
             cout << studentuSarasas[i].getVidurkis() << "\n";
           }
-        } else {
+        }
+        else
+        {
           cout << "\nNieko nerasta!\n";
         }
-      } else if (input.compare("3") == 0) {
+      }
+      else if (input.compare("3") == 0)
+      {
         //apsiraÅ¡omas gautÅ³ studentu sÄ…raÅ¡ui saugoti
         Studentas studentuSarasas[maxStud];
         int kiek;
@@ -136,29 +161,37 @@ int main() {
         cout << "\nÄ®veskite vardÄ…, pavardÄ™ ir grupÄ™ (atskirti tarpu)\n";
         cin >> vardasTemp >> pavardeTemp >> grupeTemp;
         //suieÅ¡ko studento pagal vardÄ…, pavardÄ™ ir grupÄ™
-        ieskotiStud(studentai, n, studentuSarasas, kiek, vardasTemp,pavardeTemp, grupeTemp);
-        if (kiek > 0) {
+        ieskotiStud(studentai, n, studentuSarasas, kiek, vardasTemp, pavardeTemp, grupeTemp);
+        if (kiek > 0)
+        {
           cout << "\nRasti studentas(-ai) pagal vardÄ…, pavardÄ™ ir grupÄ™:\n";
-          for (int i = 0; i < kiek; i++) {
+          for (int i = 0; i < kiek; i++)
+          {
             cout << studentuSarasas[i].getVardas() << " ";
             cout << studentuSarasas[i].getPavarde() << " ";
             cout << studentuSarasas[i].getGrupe() << " ";
             cout << studentuSarasas[i].getVidurkis() << "\n";
           }
-        } else {
+        }
+        else
+        {
           cout << "\nNieko nerasta!\n";
         }
-      } else if (input.compare("4") == 0) {
+      }
+      else if (input.compare("4") == 0)
+      {
         //ivedama is klaviatÅ«ros palei kÄ… nori atlikti rÅ«Å¡iavimÄ…
         int vidurkiArPavarde;
         cout << "\n1. Pagal vidurkÄ¯.\n2. Pagal pavardÄ™.\n\n";
         cout << "Komanda: ";
-        cin  >> vidurkiArPavarde;
+        cin >> vidurkiArPavarde;
         //iÅ¡rikiuoja studentu masyve
         rikiuoti(studentai, n, vidurkiArPavarde);
         //isspausdina studentus
         spausdintiStud(studentai, n);
-      } else if (input.compare("5") == 0) {
+      }
+      else if (input.compare("5") == 0)
+      {
         cout << "\nIveskite studento varda, pavarde, grupe ir vidurki (Atskirti tarpu).\n";
         string vardasTemp;
         string pavardeTemp;
@@ -178,16 +211,19 @@ int main() {
   return 0;
 }
 
-bool skaityti(Studentas studentai[], int &n) {
+bool skaityti(Studentas studentai[], int &n)
+{
   //atidaromas failas
   ifstream FD;
   FD.open(duom);
   //patikrina ar pavyko atidaryti faila
-  if (FD.is_open()) {
+  if (FD.is_open())
+  {
     cout << "\nNuskaitomas studentÅ³ sÄ…raÅ¡as" << endl;
     n = 0;
     //skaito kol baigsis failas
-    while (!FD.eof()) {
+    while (!FD.eof())
+    {
       //nuskaitomos reiksmes ir priskiriamos studentams
       string vardasTemp;
       string pavardeTemp;
@@ -201,27 +237,35 @@ bool skaityti(Studentas studentai[], int &n) {
       n++;
     }
     FD.close();
-    cout << "Nuskaityta sÄ—kmingai" << "\n\n";
+    cout << "Nuskaityta sÄ—kmingai"
+         << "\n\n";
     return true;
-  } else {
-    cout << "\nNerastas duomenÅ³ failas" << "\n\n";
+  }
+  else
+  {
+    cout << "\nNerastas duomenÅ³ failas"
+         << "\n\n";
     return false;
   }
 }
 
-void pildyti(Studentas studentai[], int n) {
+void pildyti(Studentas studentai[], int n)
+{
   ofstream FR;
   FR.open(duom, fstream::app);
 
-  FR << "\n" << studentai[n].getVardas() << " " << studentai[n].getPavarde() << " " << studentai[n].getGrupe() << " " << studentai[n].getVidurkis();
+  FR << "\n"
+     << studentai[n].getVardas() << " " << studentai[n].getPavarde() << " " << studentai[n].getGrupe() << " " << studentai[n].getVidurkis();
 
   FR.close();
 }
 
-void spausdintiStud(Studentas studentai[], int n) {
+void spausdintiStud(Studentas studentai[], int n)
+{
   //spasudina studentu saraÅ¡Ä…
   cout << "StudentÅ³ sÄ…raÅ¡as:" << endl;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cout << studentai[i].getVardas() << " ";
     cout << studentai[i].getPavarde() << " ";
     cout << studentai[i].getGrupe() << " ";
@@ -229,70 +273,92 @@ void spausdintiStud(Studentas studentai[], int n) {
   }
 }
 
-void ieskGerBlogStud(Studentas studentai[], int n, Studentas studentaiGerBlog[], int &kiekGeru, int &kiekBlogu) {
+void ieskGerBlogStud(Studentas studentai[], int n, Studentas studentaiGerBlog[], int &kiekGeru, int &kiekBlogu)
+{
   //suieskoma koks yra didÅ¾iausias ir maÅ¾iausias vidurkis
   double maxVid = 0;
   double minVid = 10;
-  for (int i = 0; i < n; i++) {
-    if (studentai[i].getVidurkis() > maxVid) {
+  for (int i = 0; i < n; i++)
+  {
+    if (studentai[i].getVidurkis() > maxVid)
+    {
       maxVid = studentai[i].getVidurkis();
     }
 
-    if (studentai[i].getVidurkis() < minVid) {
+    if (studentai[i].getVidurkis() < minVid)
+    {
       minVid = studentai[i].getVidurkis();
     }
   }
   // suieskomi geri ir blogi studentai ir issaugomi i atskirÄ… masyva
   kiekGeru = 0;
-  for (int i = 0; i < n; i++) {
-    if (studentai[i].getVidurkis() == maxVid) {
+  for (int i = 0; i < n; i++)
+  {
+    if (studentai[i].getVidurkis() == maxVid)
+    {
       studentaiGerBlog[kiekGeru] = studentai[i];
       kiekGeru++;
     }
   }
   kiekBlogu = 0;
-  for (int i = 0; i < n; i++) {
-    if (studentai[i].getVidurkis() == minVid) {
+  for (int i = 0; i < n; i++)
+  {
+    if (studentai[i].getVidurkis() == minVid)
+    {
       studentaiGerBlog[kiekGeru + kiekBlogu] = studentai[i];
       kiekBlogu++;
     }
   }
 }
 
-void ieskotiStud(Studentas studentai[], int n, Studentas studentuSarasas[], int &kiek, string vardasTemp, string pavardeTemp) {
+void ieskotiStud(Studentas studentai[], int n, Studentas studentuSarasas[], int &kiek, string vardasTemp, string pavardeTemp)
+{
   // iesko studentÅ³ kurie atitinka kriterijus
   kiek = 0;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     //patikrina varda ir pavardÄ™
-    if (studentai[i].getVardas().compare(vardasTemp) == 0 && studentai[i].getPavarde().compare(pavardeTemp) == 0) {
+    if (studentai[i].getVardas().compare(vardasTemp) == 0 && studentai[i].getPavarde().compare(pavardeTemp) == 0)
+    {
       studentuSarasas[kiek] = studentai[i];
       kiek++;
     }
   }
 }
 
-void ieskotiStud(Studentas studentai[], int n, Studentas studentuSarasas[], int &kiek, string vardasTemp, string pavardeTemp, string grupeTemp) {
+void ieskotiStud(Studentas studentai[], int n, Studentas studentuSarasas[], int &kiek, string vardasTemp, string pavardeTemp, string grupeTemp)
+{
   // iesko studentÅ³ kurie atitinka kriterijus
   kiek = 0;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     //patikrina varda, pavardÄ™ ir grupe
-    if (studentai[i].getVardas().compare(vardasTemp) == 0 && studentai[i].getPavarde().compare(pavardeTemp) == 0 && studentai[i].getGrupe().compare(grupeTemp) == 0) {
+    if (studentai[i].getVardas().compare(vardasTemp) == 0 && studentai[i].getPavarde().compare(pavardeTemp) == 0 && studentai[i].getGrupe().compare(grupeTemp) == 0)
+    {
       studentuSarasas[kiek] = studentai[i];
       kiek++;
     }
   }
 }
 
-void rikiuoti(Studentas studentai[], int n, int vidurkiArPavarde) {
+void rikiuoti(Studentas studentai[], int n, int vidurkiArPavarde)
+{
   //isrikiuoja masyva pagal kas yra pasirinkta, 1 - pagal vidurki; 2 - pagal pavardÄ™
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = i; j < n; j++) {
-      if (vidurkiArPavarde == 1) {
-        if (studentai[i].getVidurkis() < studentai[j].getVidurkis()) {
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = i; j < n; j++)
+    {
+      if (vidurkiArPavarde == 1)
+      {
+        if (studentai[i].getVidurkis() < studentai[j].getVidurkis())
+        {
           swap(studentai[i], studentai[j]);
         }
-      } else if (vidurkiArPavarde == 2) {
-        if (studentai[i].getPavarde().compare(studentai[j].getPavarde()) > 0) {
+      }
+      else if (vidurkiArPavarde == 2)
+      {
+        if (studentai[i].getPavarde().compare(studentai[j].getPavarde()) > 0)
+        {
           swap(studentai[i], studentai[j]);
         }
       }
