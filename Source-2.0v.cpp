330816e01a1881f8a9b000b62cf49fd389a0f2d7
kornelijusp <kornelijus.petronis2000@gmail.com>
2020-03-05 12:43:32 +0200

    I think  it works! 12.3.43v 100% Progress!

--- Source-2.0v.cpp
+++ Source-2.0v.cpp
@@ -1,15 +1,21 @@
+// Biblioteku sarasas
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
-const char dataBase[] = "data.csv";
+// GLOBALIEJI KINTAMIEJI
 
+// Duomenu failo globalus pavadinimas
+const char dataBase[] = "data.csv";
+// Maksimalus studentu kiekis galimas programoje
 const int maxStudent = 210;
 
+// Kandidatu klase
 class Kandidatas
 {
+    // Klases Kintamieji
 private:
     string vardas;
     string pavarde;
@@ -18,6 +24,7 @@ private:
     double norimasAtlig;
 
 public:
+    // Konstruktoriai
     Kandidatas()
     {
         vardas = "-";
@@ -34,6 +41,7 @@ public:
         this->darboPatirtis = darboPatirtis;
         this->norimasAtlig = norimasAtlig;
     }
+    // Klases metodu prototipai
     void setVardas(string);
     void setPavarde(string);
     void setProfesija(string);
@@ -47,6 +55,8 @@ public:
     double getNorimasAtlig();
 };
 
+// Klases metodai
+// SET
 void Kandidatas::setVardas(string vardas)
 {
     this->vardas = vardas;
@@ -68,6 +78,7 @@ void Kandidatas::setNorimasAtlig(double norimasAtlig)
     this->norimasAtlig = norimasAtlig;
 }
 //----
+// GET
 string Kandidatas::getVardas()
 {
     return vardas;
@@ -88,28 +99,36 @@ double Kandidatas::getNorimasAtlig()
 {
     return norimasAtlig;
 }
+
 // Prototipai
 double vidurkis(Kandidatas[], int);
 bool skaityti(Kandidatas[], int &);
 void paieska(Kandidatas[], int, Kandidatas[], int &, string, string, string, int, string);
 void removeDupWord(char[], string[]);
 void pildyti(Kandidatas[], int);
-
+void rikiuoti(Kandidatas[], int, int);
+void Spausdinti(Kandidatas[], int);
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// MAIN -------------------------------------------
 int main()
 {
+    // Kintamieji ir klases
     Kandidatas kandidatai[maxStudent];
+    Kandidatas kandidatuSarasas[maxStudent];
+
     int n, darboPatirtis_t, kiek;
     string state, x;
     string vardas_t, pavarde_t, profesija_t;
     double norimasAtlig_t;
-    Kandidatas kandidatuSarasas[maxStudent];
-    size_t pos = 0;
-    // string
 
+    // Patikrina ar yra failas
     if (skaityti(kandidatai, n))
     {
+        // vykdo programa
         do
         {
+            // User interface
+            // Dialogas su naudotoju
             cout << "Programa kandidatai" << endl
                  << endl;
             cout << "1. Pageidaujamo atlygio vidurkio skaičiavimas" << endl;
@@ -120,58 +139,46 @@ int main()
             cout << "6. Baigti.\n\n:";
             cin >> state;
             cout << "-------------------------------------\n";
-
+            // Switch funkcija valdo procesus
             switch (stoi(state))
             {
-                // Vidurkis
+                // Apskaiciuoja vidurki
             case 1:
                 cout << "Vidurkis: " << vidurkis(kandidatai, n) << endl;
                 break;
+                // Paieskos variklis
+                // Search engine
             case 2:
+                // UI
                 cout << "Paieska\n";
                 cout << "1. Pagal varda, pavarde\n";
                 cout << "2. Pagal varda, pavarde, profesija, darbo patirti\n";
                 cout << "3. Baigti\n\n:";
                 cin >> x;
                 cout << "-------------------------------------\n";
+                // Iesko paga 2 parametrus
                 if (!x.compare("1"))
                 {
                     cout << "\nIvesk varda ir pavarde" << endl;
                     cin >> vardas_t >> pavarde_t;
                     cout << "=====================================\n";
                     paieska(kandidatai, n, kandidatuSarasas, kiek, vardas_t, pavarde_t, profesija_t, darboPatirtis_t, x);
-                    for (int i = 0; i < kiek; i++)
-                    {
-                        cout << "\n"
-                             << i + 1 << ". "
-                             << kandidatuSarasas[i].getVardas() << " ";
-                        cout << kandidatuSarasas[i].getPavarde() << " ";
-                        cout << kandidatuSarasas[i].getProfesija() << " ";
-                        cout << kandidatuSarasas[i].getDarboPatirtis() << " ";
-                        cout << kandidatuSarasas[i].getNorimasAtlig() << endl;
-                    }
+                    Spausdinti(kandidatuSarasas, kiek);
                 }
+                // Iesko pagal 4 parametrus
                 else if (!x.compare("2"))
                 {
                     cout << "\nIvesk varda, pavarde, profesija ir darbo patirti" << endl;
                     cin >> vardas_t >> pavarde_t >> profesija_t >> darboPatirtis_t;
-                    cout << "=====================================\n";
+                    cout << "=====================================\n\n";
                     paieska(kandidatai, n, kandidatuSarasas, kiek, vardas_t, pavarde_t, profesija_t, darboPatirtis_t, x);
-                    for (int i = 0; i < kiek; i++)
-                    {
-                        cout << "\n"
-                             << i + 1 << ". "
-                             << kandidatuSarasas[i].getVardas() << " ";
-                        cout << kandidatuSarasas[i].getPavarde() << " ";
-                        cout << kandidatuSarasas[i].getProfesija() << " ";
-                        cout << kandidatuSarasas[i].getDarboPatirtis() << " ";
-                        cout << kandidatuSarasas[i].getNorimasAtlig() << endl;
-                    }
+                    Spausdinti(kandidatuSarasas, kiek);
                 }
                 else
                     break;
 
                 break;
+                // Iterpimo procesas kai norima prideti kandidata
             case 3:
                 cout << "\nIveskite studento varda, pavarde, profesija, darbo patirti ir norima atlygi (Atskirti tarpais).\n";
 
@@ -183,6 +190,28 @@ int main()
                 n++;
                 cout << "\nIrasyta sekmingai!\n";
                 break;
+                // Rikiavimas
+            case 4:
+                // UI
+                cout << "Rikiavimas pagal darbo patirti arba norima atlygi\n\n";
+                cout << "1. Pagal darbo patirti\n";
+                cout << "2. Pagal norima atlygi\n";
+                cout << "3. Baigti\n\n:";
+                cin >> x;
+                cout << "\n\n";
+                // Exit
+                if (!x.compare("3"))
+                    break;
+                // Rikiuoja pagal darbo patiri arba norima atligi
+                rikiuoti(kandidatai, n, stoi(x) - 1);
+                Spausdinti(kandidatai, n);
+
+                break;
+                // Isspausdina visus kandidatus
+            case 5:
+                Spausdinti(kandidatai, n);
+                break;
+                // Palieka programa
             case 6:
                 cout << "Programa baigta!\n";
                 break;
@@ -197,7 +226,9 @@ int main()
     return 0;
 }
 //---------------------------------------------------
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
 // Funkcijos
+// Skaiciuoja vidurki
 double vidurkis(Kandidatas kandidatai[], int n)
 {
     double suma = 0;
@@ -207,6 +238,7 @@ double vidurkis(Kandidatas kandidatai[], int n)
     }
     return suma / n;
 }
+// Skaito is failo
 bool skaityti(Kandidatas kandidatai[], int &n)
 {
     ifstream fin;
@@ -242,7 +274,7 @@ bool skaityti(Kandidatas kandidatai[], int &n)
         return false;
     }
 }
-
+// Vykdo paieska
 void paieska(Kandidatas kandidatai[], int n, Kandidatas kandidatuSarasas[], int &kiek, string vardas_t, string pavarde_t, string profesija_t, int darboPatirtis_t, string x)
 {
 
@@ -262,6 +294,7 @@ void paieska(Kandidatas kandidatai[], int n, Kandidatas kandidatuSarasas[], int 
         }
     }
 }
+// Papildo kandidatu sarasa
 void pildyti(Kandidatas kandidatai[], int n)
 {
     ofstream fout;
@@ -271,4 +304,34 @@ void pildyti(Kandidatas kandidatai[], int n)
          << kandidatai[n].getVardas() << " " << kandidatai[n].getPavarde() << " " << kandidatai[n].getProfesija() << " " << kandidatai[n].getDarboPatirtis() << " " << kandidatai[n].getNorimasAtlig();
 
     fout.close();
+}
+// Rikiavimas
+// State 1 patirtis
+// State 0 Atlygis
+void rikiuoti(Kandidatas kandidatai[], int n, int state)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+
+            if ((!state && kandidatai[i].getDarboPatirtis() < kandidatai[j].getDarboPatirtis()) || (state && kandidatai[i].getNorimasAtlig() < kandidatai[j].getNorimasAtlig()))
+            {
+                swap(kandidatai[i], kandidatai[j]);
+            }
+        }
+    }
+}
+// Spausdina klase
+void Spausdinti(Kandidatas kandidatai[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << i + 1 << ". "
+             << kandidatai[i].getVardas() << " ";
+        cout << kandidatai[i].getPavarde() << " ";
+        cout << kandidatai[i].getProfesija() << " ";
+        cout << kandidatai[i].getDarboPatirtis() << " ";
+        cout << kandidatai[i].getNorimasAtlig() << endl;
+    }
 }
